653603da5074e6e3b90dd1f34e91cfa512175f59
Kammela15Manasa <100776386+Kammela15Manasa@users.noreply.github.com>
2022-03-29 21:09:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,252 @@
+console.log("This is Atharva")
+
+function BerthFinder (BerthNo) {
+    var answer = " "
+    switch(BerthNo) {
+        case 1 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 2 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 3 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 4 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 5 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 6 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 7 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 8 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break
+        case 9 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 10 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 11 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 12 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 13 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 14 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 15 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 16 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break
+        case 17 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 18 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 19 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 20 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 21 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 22 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 23 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 24 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break 
+        case 25 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 26 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 27 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 28 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 29 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 30 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 31 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 32 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break
+        case 25 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 26 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 27 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 28 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 29 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 30 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 31 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 32 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break
+        case 33 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 34 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 35 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 36 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 37 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 38 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 39 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 40 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break
+        case 41 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 42 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 43 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 44 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 45 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 46 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 47 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 48 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break  
+        case 49 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 50 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 51 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 52 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 53 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 54 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 55 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 56 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break 
+        case 57 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 58 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 59 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 60 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 61 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 62 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 63 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 64 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break 
+        case 65 : 
+            answer = "Your Berth is Lower Berth"
+            break
+        case 66 : 
+            answer = "Your Berth is Middle Berth"
+            break
+        case 67 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 68 : 
+            answer = "Your Berth is Lower Berth"
+            break 
+        case 69 : 
+            answer = "Your Berth is Middle Berth"
+            break 
+        case 70 : 
+            answer = "Your Berth is Upper Berth"
+            break 
+        case 71 : 
+            answer = "Your Berth is Side-Lower Berth"
+            break 
+        case 72 : 
+            answer = "Your Berth is Side-Upper Berth"
+            break                                                                                                                                                                         
+        default :
+            answer = "Not in This Compartment, Please check Next"    
+    }
+    return answer
+}
+
+console.log(BerthFinder(69))
